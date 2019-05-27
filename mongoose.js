2338ca94f8580d897cada7044d05dd5fa51373a6
yosef brandwein <yosefdbrandwein@gmail.com>
2019-05-27 16:15:26 -0400

    connecting db

--- mongoose.js
+++ mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://heroku_1kbpzwdp:kjbc5kmvnau0ivqonpljehg8kp@ds261296.mlab.com:61296/heroku_1kbpzwdp',{ useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true})
      .then( () => console.log('connected to MongoDb...'))
      .catch( (e)=> console.error('Could not connect to MongoDb...', e))
 
